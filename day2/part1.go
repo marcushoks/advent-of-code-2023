abd497d853e2ff88fb3ab47785022c309a5a7f9a
Marcus Ho <marcushoks@gmail.com>
2023-12-11 21:55:21 +0800

    Complete day 2 part 1

--- day2/part1.go
+++ day2/part1.go
@@ -1,15 +1,21 @@
 package day2
 
-import (
-	"regexp"
-)
-
 func Part1(bytes []byte) int {
 	MAX_RED := 12
 	MAX_GREEN := 13
 	MAX_BLUE := 14
 
-	match := regexp.MustCompile("[0-9]+:").Find(bytes)
-	gameId := match[:len(match)-1] // removes trailing ":"
+	game := ParseGame(bytes)
+	legalSamples := 0
+	for _, s := range game.samples {
+		if s.red > MAX_RED || s.green > MAX_GREEN || s.blue > MAX_BLUE {
+			break
+		}
+		legalSamples++
+	}
 
+	if legalSamples == len(game.samples) {
+		return game.id
+	}
+	return 0
 }
---
+++ day2/part1_test.go
@@ -0,0 +1,25 @@
+package day2
+
+import (
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := [][]byte{
+		[]byte("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+		[]byte("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+		[]byte("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
+		[]byte("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
+		[]byte("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
+	}
+
+	sum := 0
+	for _, bytes := range input {
+		sum += Part1(bytes)
+	}
+
+	expected := 8
+	if sum != expected {
+		t.Errorf("Expected: %v, got: %v", expected, sum)
+	}
+}
